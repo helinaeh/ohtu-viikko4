fe9c169778363dcd3638e51ab8f7bff5218c287c
Helina Hakala <helina.hakala@gmail.com>
2013-04-21 14:43:50 +0300

    Tehtavat 1-11, vika kesken

---
+++ Verkkokauppa4/src/test/java/KauppaTest.java
@@ -0,0 +1,94 @@
+import ohtu.verkkokauppa.Kauppa;
+import ohtu.verkkokauppa.Pankki;
+import ohtu.verkkokauppa.Tuote;
+import ohtu.verkkokauppa.Varasto;
+import ohtu.verkkokauppa.Viitegeneraattori;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+
+    public KauppaTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void ostoksenPaaytyttyapankinMetodiaTilisiirtoKutsutaan() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(1);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), anyInt());
+    }
+
+    @Test
+    public void ostoksenPaaytyttyapankinMetodiaTilisiirtoKutsutaanOikeillaArvoilla() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(1);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto("pekka", viite.uusi(), "12345", k.getKaupanTili(), 5);
+        //parametreina: String nimi, int viitenumero, String tililta, String tilille, int summa
+    }
+    
+//    @Test
+//    public void hello() {
+//    }
+//    
+//    @Test
+//    public void hello() {
+//    }
+//    
+//    @Test
+//    public void hello() {
+//    }
+//    
+//    @Test
+//    public void hello() {
+//    }
+//    
+//    @Test
+//    public void hello() {
+//    }
+//    
+//    @Test
+//    public void hello() {
+//    }
+}
